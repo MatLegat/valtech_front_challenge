5e731e2d8a94102769bb837537ee6e5b83514b97
Matteus Legat <matteuslegat@gmail.com>
2018-08-06 01:23:17 -0300

    Watch static and SASS files on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const gulp = require('gulp')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
 
 
 // Configure a Fractal instance:
@@ -10,15 +12,43 @@ const fractal = require('@frctl/fractal').create()
 fractal.set('project.title', 'Valtech Card Component Library')
 fractal.web.set('builder.dest', 'build')
 fractal.docs.set('path', `${__dirname}/src/docs`)
+fractal.web.set('static.path', `${__dirname}/build/public`)
 fractal.components.set('path', `${__dirname}/src/components`)
+fractal.components.set('default.preview', '@preview')
 
 
 // Fractal CLI console utility:
 const logger = fractal.cli.console
 
 
-// Compile and export project static assets:
-const compileAssets = () => {
+// Compile Sass styles:
+const sassPath = `${__dirname}/src/components/**/*.scss`
+const compileSass = () => {
+  gulp.src(sassPath)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('build/public/css'))
+}
+const watchSass = () => {
+  gulp.watch([sassPath])
+    .on('change', compileSass)
+}
+
+
+// Copy static files to build path
+const staticPath = `${__dirname}/src/static/*`
+const copyStatic = () => {
+  gulp.src(staticPath)
+    .pipe(gulp.dest(`build/public/static/`))
+}
+const watchStatic = () => {
+  gulp.watch([staticPath])
+    .on('change', copyStatic)
+}
+
+
+// Compile and export project fractal assets:
+const buildFractalAssets = () => {
   const builder = fractal.web.builder()
   builder.on(
     'progress',
@@ -47,5 +77,11 @@ const start = () => {
 
 // Gulp build command:
 gulp.task('build', () => {
-  compileAssets().then(start)
+  buildFractalAssets().then(() => {
+    compileSass()
+    watchSass()
+    copyStatic()
+    watchStatic()
+    start()
+  })
 })
