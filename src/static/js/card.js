63924147b61f5b657f784d47a1d7708f24e05fa2
Matteus Legat <matteuslegat@gmail.com>
2018-08-08 01:08:31 -0300

    Buttons

--- src/static/js/card.js
+++ src/static/js/card.js
@@ -1,10 +1,14 @@
-const captionMovement = 120
-const textMovement = 25
+const captionMovement = 0.25
+const textMovement = 0.85
 let activeCards = []
 
 const getChildDivs = (card) =>
   Array.from(card.getElementsByClassName('content')[0].childNodes)
     .filter(node => node.tagName == 'DIV')
+
+const getBtn = (card) =>
+  Array.from(card.getElementsByClassName('content')[0].childNodes)
+    .filter(node => node.tagName == 'BUTTON')[0]
 
 const mouseOverAnimation = (card) => {
   if (activeCards.includes(card)) {
@@ -14,15 +18,21 @@ const mouseOverAnimation = (card) => {
   const childDivs = getChildDivs(card)
   const caption = childDivs[0]
   const text = childDivs[1]
+  const button = getBtn(card)
 
   const currentCaptionTop = (parseInt(getComputedStyle(caption).top))
-  caption.style.top = currentCaptionTop - captionMovement + 'px'
+  caption.style.top = currentCaptionTop * captionMovement + 'px'
 
   const currentTextTop = (parseInt(getComputedStyle(caption).top))
+  text.style.top = currentTextTop * textMovement + 'px'
   text.style.opacity = 1
-  text.style.top = currentTextTop - textMovement + 'px'
 
-  console.log(caption)
+  setTimeout(() => {
+    if (!activeCards.includes(card)) {
+      return
+    }
+    button.style.opacity = 1
+  }, 400)
 }
 
 const mouseOutAnimation = (card, event) => {
@@ -30,9 +40,12 @@ const mouseOutAnimation = (card, event) => {
   const childDivs = getChildDivs(card)
   const caption = childDivs[0]
   const text = childDivs[1]
+  const button = getBtn(card)
+
   caption.style.top = ''
   text.style.top = ''
   text.style.opacity = 0
+  button.style.opacity = 0
 }
 
 window.onload = () => {
