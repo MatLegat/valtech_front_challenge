e03bbf4b7c9128cd47f6abe6a976cb692efd1b47
Matteus Legat <matteuslegat@gmail.com>
2018-08-07 22:27:00 -0300

    Lazy image loading

---
+++ src/static/js/lazy_loader.js
@@ -0,0 +1,29 @@
+
+const loadImage = (img) => {
+  img.setAttribute('src', img.getAttribute('data-src'))
+  img.onload = () => {
+    img.removeAttribute('data-src')
+  }
+}
+
+const loadImagesOnViewport = () => {
+  const viewportHeight = window.innerHeight || document.documentElement.clientHeight
+  const imgElements = Array.from(document.getElementsByTagName('img'))
+  const notLoadedImages = imgElements.filter(img => img.getAttribute('data-src'))
+  const imagesToLoad = notLoadedImages.filter(img => {
+    const imgBoundingRect = img.parentElement.getBoundingClientRect()
+    return imgBoundingRect.top < viewportHeight && imgBoundingRect.bottom > 0
+  })
+  imagesToLoad.forEach(loadImage)
+}
+
+window.onscroll = loadImagesOnViewport
+window.onresize = loadImagesOnViewport
+
+const oldOnload = window.onload
+window.onload = () => {
+  if (typeof(oldOnload) == 'function') {
+    oldOnload()
+  }
+  loadImagesOnViewport()
+}
